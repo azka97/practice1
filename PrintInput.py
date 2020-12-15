a9fae20a0b1252aa73a808779ccdd7c281553f14
azka97 <75965214+azka97@users.noreply.github.com>
2020-12-15 12:21:02 +0700

    Add files via upload
    1st time learning python
    
    all new stuff like hello world and stuff

---
+++ PrintInput.py
@@ -0,0 +1,14 @@
+#input() will by default as String
+#basic math same as other language, which is +,-,/,*
+#exponent in python notated by '**'
+#There's '//' which is used for devided number but until how many times it will be reach the first number. Was called ' Integer Division'
+#Modulus operator notated by '%'. This is the remain number of some formula. example: 7 % 3 = 1 because 7/3= 6 with one remain number which is "1" or its called remainder
+#'type' for calling what type of a Variable is
+
+print('Pick a number :')
+num1 = input()
+print('Pick a number again :')
+num2 = input()
+print(type(num2))
+Jumlah = int(num1)+int(num2)
+print(Jumlah)
