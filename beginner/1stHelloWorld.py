a842b72d1c4926969ec5ffbb06bf00afd927bfab
azka97 <75965214+azka97@users.noreply.github.com>
2020-12-15 12:32:40 +0700

    Add files via upload

---
+++ beginner/1stHelloWorld.py
@@ -0,0 +1,14 @@
+print("Hello world")
+
+x = "Hello world 2"
+print(x)
+#Type of Variables are Integer(int),String(str),Decimal(float),TrueFalse(bool)
+#Integer for number, write it without ""/''
+#String for string variables, write it with ''/""
+#float for decimal, like integer
+#bool for True and False, should wrote it with Capital on first letter
+#Capitals do matters in python
+
+print('Hello, Who are you?')
+name = input()
+print('oh your name is',name)
---
+++ beginner/Conditions.py
@@ -0,0 +1,8 @@
+#type of conditions for bool types of Variables are >,<,==,!=
+#'>'= greater than, '<' = Lower Than, '==' = equals to, '!=' not equal to 
+#'=' for declare variables and not same as '=='
+
+print(1>7)
+print(2*9/3<5)
+print('hopes'=='Hopes')
+print(5*4!=13)
